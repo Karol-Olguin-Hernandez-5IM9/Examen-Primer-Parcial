6023c0480bb1cc300f12d6e55ba37d0db5e8cc82
Karol Olguin Hernandez 5IM9 <42345714+Karol-Olguin-Hernandez-5IM9@users.noreply.github.com>
2018-09-13 09:04:30 -0500

    Add files via upload

---
+++ ExamenP1/src/java/Servelt/Se.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servelt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.xml.ws.http.HTTPException;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Se extends HttpServlet{
+     private float num1;
+    private float num2;
+    private String con;
+    
+    public void doPost(ServletRequest peticion, ServletResponse respuesta)
+            throws IOException, HTTPException
+    {
+        num1=Float.parseFloat(peticion.getParameter("Pesos"));
+        con=peticion.getParameter("Cambio");
+        
+        if(con.equals("Dolares")){
+            num2=num1/20;
+        }
+        else
+            num2=num1*20;
+        
+        PrintWriter out= new PrintWriter(respuesta.getOutputStream());
+        out.println("<html>");
+        out.println("<head><title>Dame todo tu dinero</title></head><body>");
+        out.println("<h2>Tu dinero en"+ con + "es: " + num2 + "</h2>");
+        out.println("¡Gracias por gastar Dinero con nosotros!");
+        out.println("</body");
+        out.println("</html>");
+    }
+}
